e092b32dc3dfcf6dc17661f6a578c52a824cd356
olfredos6 <alfred.nehemie@gmail.com>
2020-09-14 16:15:08 +0200

    adds flickity.js

--- js/main.js
+++ js/main.js
@@ -152,46 +152,25 @@
     return false;
   });
 
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
 
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
 
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
-    // Initiate venobox (lightbox feature used in portofilo)
-    $(document).ready(function() {
-      $('.venobox').venobox();
-    });
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    autoPlay: true,
   });
 
   // Init AOS
-  function aos_init() {
-    AOS.init({
-      duration: 1000,
-      easing: "ease-in-out",
-      once: true
-    });
-  }
-  $(window).on('load', function() {
-    aos_init();
-  });
+  // function aos_init() {
+  //   AOS.init({
+  //     duration: 1000,
+  //     easing: "ease-in-out",
+  //     once: true
+  //   });
+  // }
+  // $(window).on('load', function() {
+  //   aos_init();
+  // });
 
 })(jQuery);
